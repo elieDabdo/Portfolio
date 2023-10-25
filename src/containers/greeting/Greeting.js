e95d25fe5c3e48c7e664b5b0f597ece2f8678ac0
Elie <elie.ad@outlook.com>
2023-10-25 01:15:21 -0400

    ll

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -2,7 +2,7 @@ import React, {useContext} from "react";
 import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import "./Greeting.scss";
-import landingPerson from "../../assets/lottie/landingPerson";
+import cool_guitare from "../../assets/lottie/landingPerson";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
@@ -52,7 +52,7 @@ export default function Greeting() {
           </div>
           <div className="greeting-image-div">
             {illustration.animated ? (
-              <DisplayLottie animationData={landingPerson} />
+              <DisplayLottie animationData={cool_guitare} />
             ) : (
               <img
                 alt="man sitting on table"
